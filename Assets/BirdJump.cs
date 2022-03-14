1b64cade67b060138474474aec3c154e3a79993c
HayanYoo <hayanyoo.dev@gmail.com>
2022-03-14 22:38:25 +0900

    feat: 충돌시 게임 오버

--- Assets/BirdJump.cs
+++ Assets/BirdJump.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BirdJump : MonoBehaviour
 {
@@ -18,5 +19,10 @@ public class BirdJump : MonoBehaviour
         if(Input.GetMouseButtonDown(0)){
             rb.velocity = Vector2.up * jumpPower;
         }        
+    }
+
+// 충돌시 게임 오버
+    private void OnCollisionEnter2D(Collision2D other) {
+        SceneManager.LoadScene("GameOverScene");
     }
 }
